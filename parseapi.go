58a4a10fb68b99009840106c490c91d09ef23516
Billy O'Shall <billy@oshall.com>
2026-09-21 13:39:17 -0400

    Prepare SDK 1.3.0 with Stack inventory support

--- parseapi.go
+++ parseapi.go
@@ -18,7 +18,7 @@ import (
 )
 
 const (
-	version         = "1.1.0"
+	version         = "1.3.0"
 	defaultBaseURL  = "https://api.parseapi.com"
 	defaultTimeout  = 10 * time.Second
 	defaultRetries  = 2
